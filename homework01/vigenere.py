4d5fd334ff9a1ec50c981bfa1ed5a88d875c22e3
George <penkov.gd@gmail.com>
2022-09-30 18:07:52 +0300

    Реализована encrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,9 +10,19 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    key_sequence = ""
+    while len(key_sequence) < len(plaintext):
+        key_sequence += keyword
+    key_sequence = key_sequence[:len(plaintext)]
+    for i in range(len(plaintext)):
+        shift = ord(key_sequence[i]) - (65 if ord(key_sequence[i]) < 97 else 97)
+        c = ord(plaintext[i])
+        if ord('a') <= c <= ord('z'):    # ord(a) = 97 ord(z) = 122
+            c = (c - 97 + shift) % 26 + 97
+        if ord('A') <= c <= ord('Z'):    # ord(A) = 65 ord(Z) =
+            c = (c - 65 + shift) % 26 + 65
+        ciphertext += chr(c)
     return ciphertext
-
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
