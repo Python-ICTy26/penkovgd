c25bcd2409ae841f1ba6ad771f816559da46a2ca
George <penkov.gd@gmail.com>
2022-09-30 16:45:04 +0300

    Пройдены тесты по шифру Цезаря

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,7 +15,15 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    ord_array = [ord(c) for c in plaintext]
+    ord_array_encrypted = []
+    for c in ord_array:
+        if ord('a') <= c <= ord('z'):  # ord(a) = 97 ord(z) = 122
+            c = (c - 97 + shift) % 26 + 97
+        if ord('A') <= c <= ord('Z'):    # ord(A) = 97 ord(Z) = 122
+            c = (c - 65 + shift) % 26 + 65
+        ord_array_encrypted.append(c)
+    ciphertext = ''.join(chr(c) for c in ord_array_encrypted)
     return ciphertext
 
 
@@ -33,7 +41,15 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    ord_array_encrypted = [ord(c) for c in ciphertext]
+    ord_array_decrypted = []
+    for c in ord_array_encrypted:
+        if ord('a') <= c <= ord('z'):  # ord(a) = 97 ord(z) = 122
+            c = (c - 97 - shift) % 26 + 97
+        if ord('A') <= c <= ord('Z'):    # ord(A) = 97 ord(Z) = 122
+            c = (c - 65 - shift) % 26 + 65
+        ord_array_decrypted.append(c)
+    plaintext = ''.join(chr(c) for c in ord_array_decrypted)
     return plaintext
 
 
@@ -42,5 +58,9 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     Brute force breaking a Caesar cipher.
     """
     best_shift = 0
-    # PUT YOUR CODE HERE
+    for shift in range(1, 27):
+        plaintext = decrypt_caesar(ciphertext, shift)
+        if plaintext in dictionary:
+            best_shift = shift
+    print('Слово %s расшифровывается как: %s' %(ciphertext, decrypt_caesar(ciphertext, best_shift)))
     return best_shift
