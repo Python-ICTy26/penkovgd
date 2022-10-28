cb44686eeb0a9cbda1c4cd0ad00ee39fa1666066
George <penkov.gd@gmail.com>
2022-10-28 19:06:51 +0300

    console gui

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -1,5 +1,5 @@
 import curses
-
+import time
 from life import GameOfLife
 from ui import UI
 
@@ -10,13 +10,28 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        screen.border()
+        screen.resize(self.life.rows + 2, self.life.cols + 2)
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        for y in range(self.life.rows):
+            for x in range(self.life.cols):
+                if self.life.curr_generation[y][x] == 1:
+                    screen.addch(y + 1, x + 1, "ඞ") # \u2588
+                else:
+                    screen.addch(y + 1, x + 1, " ")
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
-        curses.endwin()
+        while True:
+            self.draw_borders(screen)
+            self.draw_grid(screen)
+            time.sleep(.3)
+            self.life.step()
+            screen.refresh()
+
+
+if __name__ == "__main__":
+    console = Console(GameOfLife((15, 40)))
+    console.run()
