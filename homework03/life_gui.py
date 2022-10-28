572859ad57a8301e394bda571b371b6766486954
George <penkov.gd@gmail.com>
2022-10-28 19:06:38 +0300

    pygame gui

--- homework03/life_gui.py
+++ homework03/life_gui.py
@@ -7,15 +7,51 @@ from ui import UI
 class GUI(UI):
     def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
         super().__init__(life)
+        self.height = self.life.rows * cell_size
+        self.width = self.life.cols * cell_size
+        self.cell_size = cell_size
+        self.speed = speed
+        self.screen_size = self.width, self.height
+        self.screen = pygame.display.set_mode(self.screen_size)
 
     def draw_lines(self) -> None:
-        # Copy from previous assignment
-        pass
+        for x in range(0, self.width, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color("gray"), (x, 0), (x, self.height))
+        for y in range(0, self.height, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color("gray"), (0, y), (self.width, y))
 
     def draw_grid(self) -> None:
-        # Copy from previous assignment
-        pass
+        for i in range(self.life.rows):
+            for j in range(self.life.cols):
+                if self.life.curr_generation[i][j] == 0:
+                    pygame.draw.rect(
+                        self.screen, pygame.Color('white'), (j * self.cell_size, i * self.cell_size, self.cell_size, self.cell_size)
+                    )
+                else:
+                    pygame.draw.rect(
+                        self.screen, pygame.Color('black'), (j * self.cell_size, i * self.cell_size, self.cell_size, self.cell_size)
+                    )
 
     def run(self) -> None:
-        # Copy from previous assignment
-        pass
+        pygame.init()
+        clock = pygame.time.Clock()
+        pygame.display.set_caption("Game of Life")
+        self.screen.fill(pygame.Color("white"))
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+
+            self.draw_grid()
+            self.draw_lines()
+            self.life.step()
+            pygame.display.flip()
+            clock.tick(self.speed)
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    gui = GUI(GameOfLife((30, 40)), 20, 7)
+    gui.run()
