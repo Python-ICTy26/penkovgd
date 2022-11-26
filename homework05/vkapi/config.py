0f57d827ebe30b1e176e4abd4892d84ce1f3feb7
George <penkov.gd@gmail.com>
2022-11-26 15:08:21 +0300

    added access token

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "vk1.a.HpJh3HbZn1IHXiK_yrKXmWbtdl5bijOgpIH8yZmO0Eoakx2RZeQjvrSEAsG9uVeTUc4CIHkp0yOKUhjLA1n690ipb7yI54koje1I753WbrZI4tRQv-y9Rw3zMQ9zs82gNKjIAC8uPaQnz_xzwmYxILRRwClrINCN5erq-nFj5C5--P5k_MHCTMSC4RdrTZJ8",
     "version": "5.126",
 }
